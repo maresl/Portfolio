8e21e78908561953a41ff5e6bf0881cdf4bd4ec0
David Underwood <davidunderwood@Davids-MacBook-Pro.local>
2022-09-08 15:55:23 -0700

    statement markup

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
+import Statement from './Components/Statement';
 
 function App() {
   return (
     <>
     <Header />
     <About />
+    <Statement />
     <Footer />
     </>
   );
